354ac28f7ea02292e0ee4776297dafab056173b8
glamostoffer <glamostoffer@gmail.com>
2023-11-02 20:05:15 +0300

    add user models

---
+++ domain/user.go
@@ -0,0 +1,24 @@
+package domain
+
+import (
+	"context"
+	"github.com/google/uuid"
+	"time"
+)
+
+type User struct {
+	ID        uuid.UUID
+	Name      string
+	Email     string
+	Password  string
+	Role      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type UserRepository interface {
+	Create(c context.Context, user *User) error
+	Fetch(c context.Context) ([]User, error)
+	GetByEmail(c context.Context, email string) (User, error)
+	GetByID(c context.Context, id string) (User, error)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module GO-FJ
 
-go 1.21
+go 1.20
+
+require github.com/google/uuid v1.4.0
