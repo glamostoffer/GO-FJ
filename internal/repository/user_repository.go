8a331f4bc2e87abf352313e15d838a8febd5519e
glamostoffer <glamostoffer@gmail.com>
2023-11-13 10:53:44 +0300

    fix user repository

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -4,7 +4,8 @@ import (
 	"GO-FJ/internal/domain"
 	"context"
 	"database/sql"
-	"time"
+	"fmt"
+	"github.com/sirupsen/logrus"
 )
 
 type userRepository struct {
@@ -16,9 +17,13 @@ func NewUserRepository(db *sql.DB) domain.UserRepository {
 }
 
 func (ur *userRepository) Create(c context.Context, user *domain.User) error {
-	var lastInsertId int
-	query := "INSERT INTO users(name, email, password, role, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6)"
-	err := ur.db.QueryRowContext(c, query, user.Name, user.Email, user.Password, "user", time.Now(), time.Now()).Scan(&lastInsertId)
+	var lastInsertId int64
+	query := "INSERT INTO users(name, email, password, role, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id"
+	err := ur.db.QueryRowContext(c, query, user.Name, user.Email, user.Password, user.Role, user.CreatedAt, user.UpdatedAt).Scan(&lastInsertId)
+	fmt.Println(lastInsertId)
+	if err != nil {
+		logrus.Errorf("cannot insert user into users: %s", err.Error())
+	}
 
 	return err
 }
@@ -28,18 +33,19 @@ func (ur *userRepository) GetByEmail(c context.Context, email string) (domain.Us
 	query := "SELECT id, name, email, password, role, created_at, updated_at FROM users WHERE email = $1"
 	err := ur.db.QueryRowContext(c, query, email).Scan(&u.ID, &u.Name, &u.Email, &u.Password, &u.Role, &u.CreatedAt, &u.UpdatedAt)
 	if err != nil {
-		return domain.User{}, nil
+		return u, nil
 	}
 
-	return u, nil
+	return u, err
 }
+
 func (ur *userRepository) GetByID(c context.Context, id string) (domain.User, error) {
 	u := domain.User{}
 	query := "SELECT id, name, email, password, role, created_at, updated_at FROM users WHERE id = $1"
 	err := ur.db.QueryRowContext(c, query, id).Scan(&u.ID, &u.Name, &u.Email, &u.Password, &u.Role, &u.CreatedAt, &u.UpdatedAt)
 	if err != nil {
-		return domain.User{}, nil
+		return u, nil
 	}
 
-	return u, nil
+	return u, err
 }
