daa400a05c490633ab0eb391b8430a5deff80506
glamostoffer <glamostoffer@gmail.com>
2023-11-21 12:36:14 +0300

    fix tokenutil

--- util/tokenutil.go
+++ util/tokenutil.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"GO-FJ/internal/domain"
+	"fmt"
 	jwt "github.com/golang-jwt/jwt/v4"
 	"github.com/sirupsen/logrus"
 	"time"
@@ -11,6 +12,7 @@ func CreateAccessToken(user *domain.User, secret string, expiry int) (accessToke
 	exp := &jwt.NumericDate{time.Now().Add(time.Hour * time.Duration(expiry))}
 	claims := &domain.JwtCustomClaims{
 		Name: user.Name,
+		Role: user.Role,
 		ID:   user.ID,
 		RegisteredClaims: jwt.RegisteredClaims{
 			ExpiresAt: exp,
@@ -23,4 +25,54 @@ func CreateAccessToken(user *domain.User, secret string, expiry int) (accessToke
 		return "", err
 	}
 	return t, err
+}
+
+func CreateRefreshToken(user *domain.User, secret string, expiry int) (refreshToken string, err error) {
+	claimsRefresh := &domain.JwtCustomRefreshClaims{
+		ID: user.ID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: &jwt.NumericDate{time.Now().Add(time.Hour * time.Duration(expiry))},
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claimsRefresh)
+	rt, err := token.SignedString([]byte(secret))
+	if err != nil {
+		logrus.Errorf("error during creating refresh token string: %s", err.Error())
+		return "", err
+	}
+	return rt, err
+}
+
+func IsAuthorized(requestToken string, secret string) (bool, error) {
+	_, err := jwt.Parse(requestToken, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secret), nil
+	})
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func ExtractIDFromToken(requestToken string, secret string) (string, error) {
+	token, err := jwt.Parse(requestToken, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+
+	if !ok && !token.Valid {
+		return "", fmt.Errorf("Invalid Token")
+	}
+
+	return claims["id"].(string), nil
 }
