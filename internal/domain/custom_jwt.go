19dfd5a804799ceeee7731fcc8df8c0aa1c867f0
glamostoffer <glamostoffer@gmail.com>
2023-11-21 12:35:53 +0300

    add role into jwt

--- internal/domain/custom_jwt.go
+++ internal/domain/custom_jwt.go
@@ -4,11 +4,13 @@ import "github.com/golang-jwt/jwt/v4"
 
 type JwtCustomClaims struct {
 	Name string `json:"name"`
+	Role string `json:"role"`
 	ID   int64  `json:"id"`
 	jwt.RegisteredClaims
 }
 
 type JwtCustomRefreshClaims struct {
-	ID string `json:"id"`
+	Role string `json:"role"`
+	ID   int64  `json:"id"`
 	jwt.RegisteredClaims
 }
