daddcee4c788a388d47851388f762e8440f49794
glamostoffer <glamostoffer@gmail.com>
2023-11-21 12:40:42 +0300

    add route

---
+++ internal/server/route.go
@@ -0,0 +1,13 @@
+package server
+
+import (
+	"database/sql"
+	"github.com/gin-gonic/gin"
+	"time"
+)
+
+func Setup(db *sql.DB, timeout time.Duration, gin *gin.Engine) {
+	publicRouter := gin.Group("")
+	NewSignupRouter(db, timeout, publicRouter)
+	NewLoginRouter(db, timeout, publicRouter)
+}
